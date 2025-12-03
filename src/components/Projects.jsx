6de1ea4b4dfc15e409d0102de87d995ffeb7e25d
Elanchezhiyan Elangovan <elanchezhiyan2440@gmail.com>
2025-12-03 10:28:02 +0530

    github Url change

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -64,47 +64,47 @@ function Projects() {
                 <ProjectTile
                     projectName={"Predictive Maintenance of PV Panels using GRU and IoT"}
                     projectDesc={"Engineered a predictive PV maintenance system using Deep Learning and IoT for MCA final year thesis."}
-                    projectGithubLink={"https://github.com/wraththevengeful/finaldraft-ss.git"}
+                    projectGithubLink={"https://github.com/elanchezhiyan2440/finaldraft-ss.git"}
                     projectDemoLink={"#"}
                     isDemoAvailable={false}
                 ></ProjectTile>
                 <ProjectTile
                     projectName={"ResuMe"}
                     projectDesc={"Built a responsive resume generator using React and Vite. Implemented complex state management to handle nested objects for education/experience arrays, utilizing controlled components for immediate UI updates."}
-                    projectGithubLink={"https://wraththevengeful.github.io/CV-Application/"}
-                    projectDemoLink={"https://wraththevengeful.github.io/CV-Application/"}
+                    projectGithubLink={"https://elanchezhiyan2440.github.io/CV-Application/"}
+                    projectDemoLink={"https://elanchezhiyan2440.github.io/CV-Application/"}
                     isDemoAvailable={true}
                 ></ProjectTile>
                 <ProjectTile
                     projectName={"Etch a Sketch"}
                     projectDesc={`An interactive browser-based sketchpad built with vanilla JavaScript. Utilizes DOM manipulation for dynamic grid generation and efficient event listeners to implement "hover-to-draw" functionality with adjustable resolution.`}
-                    projectGithubLink={"https://github.com/wraththevengeful/etch-a-sketch"}
-                    projectDemoLink={"https://wraththevengeful.github.io/etch-a-sketch/"}
+                    projectGithubLink={"https://github.com/elanchezhiyan2440/etch-a-sketch"}
+                    projectDemoLink={"https://elanchezhiyan2440.github.io/etch-a-sketch/"}
                     isDemoAvailable={true}
                 ></ProjectTile>
                 <ProjectTile
                     projectName={"My personal Scripts"}
                     projectDesc={"Scripts I write to wield the internet to my liking when things don't work as I expected like download WhatsApp Stickers and putting them to Signal Messengers."}
-                    projectGithubLink={"https://github.com/wraththevengeful/scripts"}
+                    projectGithubLink={"https://github.com/elanchezhiyan2440/scripts"}
                     projectDemoLink={"#"}
                     isDemoAvailable={false}
                 ></ProjectTile>
                 <ProjectTile
                     projectName={"Calculator"}
                     projectDesc={"The very first UI I ever developed. It ain't much but it's honest work and nostalgia. Learnt first ever JS and CSS and HTML with this. Developed multiple branches to also learn the intricacies of Git and Github Pages."}
-                    projectGithubLink={"https://github.com/wraththevengeful/calculator"}
-                    projectDemoLink={"https://wraththevengeful.github.io/calculator/"}
+                    projectGithubLink={"https://github.com/elanchezhiyan2440/calculator"}
+                    projectDemoLink={"https://elanchezhiyan2440.github.io/calculator/"}
                     isDemoAvailable={true}
                 ></ProjectTile>
                 <ProjectTile
                     projectName={"Developer Portfolio"}
                     projectDesc={"A responsive, high-performance personal portfolio website built with React and Vite. Designed to showcase projects, skills, and educational background with smooth scroll animations and an interactive UI."}
-                    projectGithubLink={"https://github.com/wraththevengeful/portfolio"}
-                    projectDemoLink={"https://wraththevengeful.github.io/portfolio/"}
+                    projectGithubLink={"https://github.com/elanchezhiyan2440/portfolio"}
+                    projectDemoLink={"https://elanchezhiyan2440.github.io/portfolio/"}
                     isDemoAvailable={true}
                 ></ProjectTile>
             </div>
-            <MoreProjectsText githubUrl={"https://github.com/wraththevengeful/"}></MoreProjectsText>
+            <MoreProjectsText githubUrl={"https://github.com/elanchezhiyan2440/"}></MoreProjectsText>
         </>
     )
 }
